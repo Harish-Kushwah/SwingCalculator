c768ba5ed5e795ab52ef5eb607792d0966672b8e
Harish-Kushwah <harishkushwah54321@gmail.com>
2023-09-08 18:21:47 +0530

    Entered numbers using keybord buttons on calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,7 @@
 import  java.awt.*;
-import javax.imageio.ImageIO;
 import  javax.swing.*;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.Border;
 import  java.awt.event.*;
-import  java.awt.image.*;
-import java.io.File;
 import java.io.IOException;
 
 class MyFrame extends JFrame
@@ -23,23 +19,24 @@ class MyFrame extends JFrame
 
     }
 }
-
-public class Calculator extends MyFrame implements ActionListener {
+public class Calculator extends MyFrame implements ActionListener , KeyListener {
 
      JPanel button_panel;
      JButton [] operators = new JButton[24];
      Container container;
      JTextArea box;
-     String button_labels[] = {"%","CE","C","clear","1/x","x^2","sqrt(x)","/","7","8","9","*","4","5","6","-","1","2","3","+","+/-","0",".","="};
-     StringBuffer ans = new StringBuffer();
+     String button_labels[] = {"%","CE","C","clear","x^-1","x^2","sqrt(x)","/","7","8","9","x","4","5","6","-","1","2","3","+"," +/-","0",".","="};
+     StringBuffer  ans ;
 
      Calculator() throws IOException {
         container = getContentPane();
+        ans = new StringBuffer();
         box = new JTextArea();
         box.setBounds(5,5,275,125);
         box.setFont(new Font("Arial",Font.BOLD,20));
         box.setLineWrap(true);
         box.setBackground(Color.white);
+        box.setEditable(false);
         container.add(box);
 
 
@@ -74,6 +71,7 @@ public class Calculator extends MyFrame implements ActionListener {
                 if(in==3)
                     operators[in].setText("<-");
 
+
                 if(in<8 || j==3) {
 
                      operators[in].setBackground(new Color(76, 76, 80));
@@ -91,17 +89,24 @@ public class Calculator extends MyFrame implements ActionListener {
                 operators[in].setPreferredSize(new Dimension(63,45));
 
                 operators[in].addActionListener(this);
+                operators[in].addKeyListener(this);
+
                 button_panel.add(operators[in],gbc);
+
+
                 in++;
 
             }
 
         }
 
+        button_panel.addKeyListener(this);
+        box.addKeyListener(this);
         container.add(button_panel);
         setVisible(true);
     }
 
+    @Override
     public  void actionPerformed(ActionEvent e)
     {
         int total_button  = operators.length;
@@ -125,15 +130,15 @@ public class Calculator extends MyFrame implements ActionListener {
         }
         else if(e.getSource()==operators[total_button-4])   //for negation
         {
-            if(ans.charAt(0)=='-')
-            {
-                  ans.replace(0,1,"+");
+            int len = ans.length();
+            if(len>0) {
+                if (ans.charAt(0) == '-') {
+                    ans.replace(0, 1, "+");
+                } else {
+                    ans.insert(0, '-');
+                }
+                box.setText(new String(ans));
             }
-            else
-            {
-                ans.insert(0,'-');
-            }
-            box.setText(new String(ans));
         }
        else if(e.getSource()==operators[6])   //for square root
         {
@@ -186,10 +191,83 @@ public class Calculator extends MyFrame implements ActionListener {
         {
            for (JButton operator : operators) {
             if (e.getSource() == operator) {
-                ans.append(operator.getText());
+                if(operator.getText().equals("x"))
+                {
+                    ans.append("*");
+                }
+                else{
+                    ans.append(operator.getText());
+                }
             }
            }
             box.setText(new String(ans));
+        }
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        for (int i = 0; i < 24; i++) {
+            if(e.getKeyChar()=='*')
+            {
+                ans.append("*");
+                box.setText(new String(ans));
+                operators[11].setBackground(new Color(125, 125, 131));
+                break;
+            }
+            else
+            if (e.getKeyChar() == operators[i].getText().charAt(0)) {
+                String st = Character.toString(e.getKeyChar());
+                ans.append(new StringBuffer(st));
+                box.setText(new String(ans));
+                operators[i].setBackground(new Color(125, 125, 131));
+                break;
+            }
+        }
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        //evaluate the answer
+        if(e.getKeyCode()==KeyEvent.VK_ENTER)
+        {
+            String a = String.valueOf(EvaluateExpression.evaluate(new String(ans)));
+            box.setText(a);
+            ans = new StringBuffer(a);
+
+        }
+        //fot clear the number by pressing backspace button
+        if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE)
+        {
+
+            int len = ans.length();
+            if(len!=0) {
+                ans.replace(len - 1, len, "");
+                box.setText(new String(ans));
+            }
+        }
+        System.out.println("I pressed");
+        System.out.println(ans);
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int in=0;
+        for(int i=0;i<6;i++)
+        {
+            for(int j=0;j<4;j++){
+                if (e.getKeyChar() == operators[in].getText().charAt(0) || e.getKeyChar()=='*' && in!=23 ) {
+                    if(in<8 || j==3 ) {
+                        operators[in].setBackground(new Color(76, 76, 80));
+                    }
+                    else{
+                        operators[in].setBackground(new Color(112, 112, 119));
+                    }
+
+                }
+                in++;
+            }
         }
     }
     public static void main(String[] args) throws IOException {
